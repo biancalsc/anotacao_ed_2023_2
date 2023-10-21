45cdeb8e864d78971230d6a929292ffc09ff9c09
biancalsc <biancalsc1409@gmail.com>
2023-10-20 21:37:50 -0300

    aula 20-10

---
+++ aula 20-10/algorithms_and_data_structures_ts/busca/string_search_view.ts
@@ -0,0 +1,39 @@
+/*
+** Trivial and Boyer-Moore string matching algorithms usage
+**
+** Author: Fabrício Galende M. de Carvalho, DSc.
+**
+*/
+
+import {StringSearch} from "./string_search"
+
+let str_search = new StringSearch();
+/*
+let pattern_1 = "fabricio";
+let pattern_2 = "são";
+let pattern_3 = "bra"
+let text = "fabricio e fabricio são xaras"
+*/
+//pos:      0          11
+
+//eu que fiz
+let text2 = "A Bianca está procurando estágio, mas sempre pedem coisas que a Bianca não tem. Bianca está muito triste."
+let pattern_4 = "Bianca"
+
+/*
+console.log(`Occurrence indices for ${pattern_1}: `, str_search.trivial(pattern_1,text));
+console.log(`Occurrence indices for ${pattern_2}: `, str_search.trivial(pattern_2,text));
+console.log(`Occurrence indices for ${pattern_3}: `, str_search.trivial(pattern_3,text));
+*/
+
+//eu que fiz
+console.log(`Occurrence indices for ${pattern_4}: `, str_search.trivial(pattern_4,text2));
+
+/*
+console.log(str_search.build_char_map("fabricio"));
+console.log(`Occurrence indices for ${pattern_1}: `, str_search.boyer_moore(pattern_1,text));
+*/
+
+//eu que fiz
+console.log(str_search.build_char_map("fabricio"));
+console.log(`Occurrence indices for ${pattern_4}: `, str_search.boyer_moore(pattern_4,text2));
